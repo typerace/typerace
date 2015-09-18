a566b1453d9da8b993b66f231c0058245b6c47ca
Blaze <b.tustanowski@gmail.com>
2015-09-18 18:30:01 +0100

    registration tests and api fixes

--- tests/api/user.js
+++ tests/api/user.js
@@ -8,12 +8,51 @@ var should = chai.should();
 var expect = chai.expect;
 chai.use(chaiHttp);
 
-it('should correctly respond to /users/check as guest', function(done) {
+it('should respond to /users/check as guest', function(done) {
     chai.request(server)
         .get('/api/users/check')
         .end(function(err, res){
             expect(err).to.be.null;
             expect(res).to.have.status(401);
+            done();
+        });
+});
+
+it('should respond to incorrect login attempts', function(done) {
+    chai.request(server)
+        .post('/api/users/login', {
+            'email': 'incorrect@email.com',
+            'password': 'password',
+        })
+        .end(function(err, res){
+            expect(err).to.be.null;
+            expect(res).to.have.status(401);
+            done();
+        });
+});
+
+it('should respond to incorrect registration attempts', function(done) {
+    chai.request(server)
+        .post('/api/users/register', {
+            'email': 'incorrectemail',
+            'password': 'password',
+        })
+        .end(function(err, res){
+            expect(err).to.be.null;
+            expect(res).to.have.status(400);
+            done();
+        });
+});
+
+it('should respond to incomplete registration attempts', function(done) {
+    chai.request(server)
+        .post('/api/users/register', {
+            'email': 'incorrect@email.com',
+            'password': 'password',
+        })
+        .end(function(err, res){
+            expect(err).to.be.null;
+            expect(res).to.have.status(400);
             done();
         });
 });
