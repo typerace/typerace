bcb1366b78f6dc591f3e9a9f8a89ec27955552d5
Blaze <b.tustanowski@gmail.com>
2015-09-24 11:34:11 +0100

    eslint baby

--- api/controllers/text.js
+++ api/controllers/text.js
@@ -4,11 +4,11 @@ var router = express.Router();
 var passport = require("passport");
 var model  = require("../models");
 
-router.get("/", passport.user,  function(req, res, next) {
+router.get("/", passport.user,  function(req, res) {
     model.text.findAll({
-        where: {user_id: req.user.id}
-    }).then(function(r) {
-        return res.json(r);
+        where: {user_id: req.user.id},
+    }).then(function(texts) {
+        return res.json(texts);
     });
 });
 
